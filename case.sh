3935b886550a3fa1f3d3e86ca1bff600ba295e32
root <root@localhost.localdomain>
2019-10-19 13:27:13 -0300

    hola

---
+++ case.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read INPUT_STRING 
+case $INPUT_STRING in 
+
+SQL) 
+  echo "Seleccione SQL" 
+;;
+Rsync)
+   echo "Sincronice con Rsync" 
+    ;;
+
+tar)
+   echo "lo siento , no respaldado"
+   ;;
+
+  esac
+
+
+####################Comienza el scrip ##################
+  echo 
+  echo "Gracias" 
+
+  
